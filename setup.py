41229184a4a5358df1dad1b808f9f300c7765950
Matt Skone <matt.skone@gmail.com>
2019-09-09 07:05:46 -0700

    Add install_requires

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=['proc_gcs_utils'],
+    install_requires=['google-cloud-storage'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
