986bba27f04115e5a89c51d5761f3547c39ebd41
Matt Skone <mattskone@users.noreply.github.com>
2020-03-18 14:03:54 -0700

    Add new copy_file() function (#6)
    * Assertion won't be called if there are no blobs in the iterator
    
    * Implement new copy_file() function

--- proc_gcs_utils/tests/test_gcs.py
+++ proc_gcs_utils/tests/test_gcs.py
@@ -6,7 +6,8 @@ from unittest.mock import patch
 
 import pytest
 
-from proc_gcs_utils.gcs import (download_file_from_gcs,
+from proc_gcs_utils.gcs import (copy_file,
+                                download_file_from_gcs,
                                 download_files_from_gcs,
                                 list_bucket_contents,
                                 list_bucket_folders,
@@ -99,14 +100,60 @@ def test_gcs_bucket_upload_download():
                             GCS_BUCKET_NAME,
                             '{0}/{1}'.format(GCS_BUCKET_PATH, filename0),
                             '{0}/renamed/{1}'.format(GCS_BUCKET_PATH, filename0))
+
+                # Find it in its new location
                 blobs = list_bucket_contents(GCP_PROJECT_NAME,
                                              GCS_BUCKET_NAME,
                                              '{}/renamed'.format(GCS_BUCKET_PATH))
+                contents = []
                 for blob in blobs:
-                    assert blob.name == '{0}/renamed/{1}'.format(GCS_BUCKET_PATH, filename0)
+                    contents.append(blob.name)
+                assert '{0}/renamed/{1}'.format(GCS_BUCKET_PATH, filename0) in contents
+
+                # Verify it was removed from its old location
+                blobs = list_bucket_contents(GCP_PROJECT_NAME,
+                                             GCS_BUCKET_NAME,
+                                             GCS_BUCKET_PATH)
+                contents = []
+                for blob in blobs:
+                    contents.append(blob.name)
+                assert '{0}/{1}'.format(GCS_BUCKET_PATH, filename0) not in contents
+
+                # Move it back
                 rename_file(GCP_PROJECT_NAME,
                             GCS_BUCKET_NAME,
                             '{0}/renamed/{1}'.format(GCS_BUCKET_PATH, filename0),
+                            '{0}/{1}'.format(GCS_BUCKET_PATH, filename0))
+
+            # Verify we can copy a test file
+            with patch('proc_gcs_utils.gcs.os.environ', test_environ):
+                copy_file(GCP_PROJECT_NAME,
+                          GCS_BUCKET_NAME,
+                          '{0}/{1}'.format(GCS_BUCKET_PATH, filename0),
+                          '{0}/copied/{1}'.format(GCS_BUCKET_PATH, filename0))
+
+                # Find it in its new location
+                blobs = list_bucket_contents(GCP_PROJECT_NAME,
+                                             GCS_BUCKET_NAME,
+                                             '{}/copied'.format(GCS_BUCKET_PATH))
+                contents = []
+                for blob in blobs:
+                    contents.append(blob.name)
+                assert '{0}/copied/{1}'.format(GCS_BUCKET_PATH, filename0) in contents
+
+                # Verify it also still exists in its old location
+                blobs = list_bucket_contents(GCP_PROJECT_NAME,
+                                             GCS_BUCKET_NAME,
+                                             GCS_BUCKET_PATH)
+                contents = []
+                for blob in blobs:
+                    contents.append(blob.name)
+                assert '{0}/{1}'.format(GCS_BUCKET_PATH, filename0) in contents
+
+                # Move it back
+                rename_file(GCP_PROJECT_NAME,
+                            GCS_BUCKET_NAME,
+                            '{0}/copied/{1}'.format(GCS_BUCKET_PATH, filename0),
                             '{0}/{1}'.format(GCS_BUCKET_PATH, filename0))
 
             # Verify we can list the subfolder(s) in the test data folder
