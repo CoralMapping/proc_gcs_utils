ab2230dc4acd8d2c46db220eb716326367c10d0c
Matt Skone <matt.skone@gmail.com>
2020-04-23 14:37:29 -0700

    WIP Able to build the package with correct name, version, and dependencies

--- setup.py
+++ setup.py
@@ -7,16 +7,17 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="proc-gcs-utils",
-    version="0.0.1",
+    version="1.0.0",
     description="Utility functions for Google Cloud Storage",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=['proc_gcs_utils'],
-    install_requires=['google-cloud-storage'],
+    install_requires=['google-cloud-storage==1.16.1'],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache Software License",
+        "License :: OSI Approved :: Apache 2.0 Software License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    url='https://github.com/CoralMapping/proc_gcs_utils'
 )
