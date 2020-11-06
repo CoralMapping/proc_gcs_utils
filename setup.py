876cc6fffa5eea3791f9b9c4941aadfb9417cc04
Matt Skone <matt.skone@gmail.com>
2020-11-06 05:58:09 -0800

    Bump version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="gcsutils",
-    version="1.1.4",
+    version="1.1.5",
     description="Utility functions for Google Cloud Storage",
     long_description=long_description,
     long_description_content_type="text/markdown",
