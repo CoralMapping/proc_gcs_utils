7dff3a11ea199647389cad3cb01f3349991fe337
Matt Skone <matt.skone@gmail.com>
2022-06-14 18:46:08 -0700

    Black formatting

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-'''
+"""
  Copyright Vulcan Inc. 2018-2020
 
  Licensed under the Apache License, Version 2.0 (the "License").
@@ -11,7 +11,7 @@
  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
  express or implied. See the License for the specific language governing
  permissions and limitations under the License.
-'''
+"""
 
 
 import setuptools
@@ -27,13 +27,13 @@ setuptools.setup(
     description="Utility functions for Google Cloud Storage",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=['gcsutils'],
-    install_requires=['google-cloud-storage==2.4.0'],
+    packages=["gcsutils"],
+    install_requires=["google-cloud-storage==2.4.0"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.10',
-    url='https://github.com/CoralMapping/proc_gcs_utils'
+    python_requires=">=3.10",
+    url="https://github.com/CoralMapping/proc_gcs_utils",
 )
